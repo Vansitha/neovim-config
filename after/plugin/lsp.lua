1fc29ba96006392155e2aceba983c50c88a60e24
Vansitha <vansitha1@gmail.com>
2024-02-04 18:26:01 +0530

    feat: add sql and c lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,60 +1,63 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- Uncomment if lua lsp is installed
-require('lspconfig').lua_ls.setup({})
+require("lspconfig").lua_ls.setup({})
 
-require('lspconfig').tsserver.setup({})
+require("lspconfig").tsserver.setup({})
 
-require('lspconfig').clangd.setup({})
+--[[ NEED TO FIX
+require("lspconfig").jdtls.setup({
+	root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
+})]]
 
-require('lspconfig').sqlls.setup({
-  cmd = {"sql-language-server", "up", "--method", "stdio"},
-  filetypes = {"sql"},
-  root_dir = function(fname)
-    return vim.fn.getcwd();
-  end
+require("lspconfig").clangd.setup({})
+
+require("lspconfig").sqlls.setup({
+	cmd = { "sql-language-server", "up", "--method", "stdio" },
+	filetypes = { "sql" },
+	root_dir = function(fname)
+		return vim.fn.getcwd()
+	end,
 })
 
-require('lspconfig').emmet_language_server.setup(
-{
-  filetypes = { "css", "html", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
-  -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-  -- **Note:** only the options listed in the table are supported.
-  init_options = {
-    --- @type string[]
-    excludeLanguages = {},
-    --- @type string[]
-    extensionsPath = {},
-    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-    preferences = {},
-    --- @type boolean Defaults to `true`
-    showAbbreviationSuggestions = true,
-    --- @type "always" | "never" Defaults to `"always"`
-    showExpandedAbbreviation = "always",
-    --- @type boolean Defaults to `false`
-    showSuggestionsAsSnippets = false,
-    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-    syntaxProfiles = {},
-    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-    variables = {},
-  },
-}
-)
+require("lspconfig").emmet_language_server.setup({
+	filetypes = { "css", "html", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+	-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+	-- **Note:** only the options listed in the table are supported.
+	init_options = {
+		--- @type string[]
+		excludeLanguages = {},
+		--- @type string[]
+		extensionsPath = {},
+		--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+		preferences = {},
+		--- @type boolean Defaults to `true`
+		showAbbreviationSuggestions = true,
+		--- @type "always" | "never" Defaults to `"always"`
+		showExpandedAbbreviation = "always",
+		--- @type boolean Defaults to `false`
+		showSuggestionsAsSnippets = false,
+		--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+		syntaxProfiles = {},
+		--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+		variables = {},
+	},
+})
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    -- confirm completion
-    ['<Tab>'] = cmp.mapping.confirm({select = true}),
+	mapping = cmp.mapping.preset.insert({
+		-- confirm completion
+		["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
-    -- scroll up and down the documentation window
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  })
+		-- scroll up and down the documentation window
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
 })
